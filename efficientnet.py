878c16d6f6782f9a4274569a4560fab9e9cea1bd
kubek22 <01171264@pw.edu.pl>
2025-03-23 11:32:53 +0100

    resnet with optimal parameters

--- efficientnet.py
+++ efficientnet.py
@@ -17,11 +17,20 @@ torch.cuda.manual_seed(SEED)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 size = (224, 224)
-# smaller batch size maybe
-# BATCH_SIZE = 256
 BATCH_SIZE = 128
 
-train_dataset, val_dataset, test_dataset = load_datasets(size)
+# optimal parameters
+learning_rate = 0.0005
+betas = (0.7, 0.997)
+weight_decay = 1e-5
+
+# augmentation
+apply_rotation = False
+apply_blur = True
+apply_brightness= True
+apply_cutout = True
+
+train_dataset, val_dataset, test_dataset = load_datasets(size, apply_rotation=apply_rotation, apply_blur=apply_blur, apply_brightness=apply_brightness, apply_cutout=apply_cutout)
 train_dataloader, val_dataloader, test_dataloader = create_data_loaders(train_dataset, val_dataset, test_dataset, BATCH_SIZE)
 
 N_CLASSES = 10
@@ -48,11 +57,11 @@ model_path = model_dir + "/efficientnet.pth"
 history_path = history_dir + "/efficientnet.pkl"
 
 n = 5
-lr = 0.001
-epochs = 10
-# epochs may be decreased to 5
+epochs = 20
+tolerance = 3
 
 start_time = time.time()
-repeat_training(n, init_efficientnet, lr, model_path, history_path, epochs, train_dataloader, val_dataloader, test_dataloader, device)
+repeat_training(n, init_efficientnet, learning_rate, model_path, history_path, epochs, train_dataloader, val_dataloader, test_dataloader, device,
+                betas=betas, weight_decay=weight_decay, tolerance=tolerance)
 end_time = time.time()
 print(f"total time: {end_time - start_time}\n")
